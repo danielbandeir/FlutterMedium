4b4b1297ae5601802794d6c373721954c7d4696c
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-11-25 23:53:55 -0300

    Changed something

--- fire_analytics/lib/analytics.dart
+++ fire_analytics/lib/analytics.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class Analytics { 
-  FirebaseAnalytics analytics = FirebaseAnalytics();
+  final FirebaseAnalytics analytics = FirebaseAnalytics();
 }
