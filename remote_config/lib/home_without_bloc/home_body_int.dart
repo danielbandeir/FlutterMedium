a49414442d8374e741071597087dc5e02006a443
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-11-26 00:11:00 -0300

    Added BOdy with get int

---
+++ remote_config/lib/home_without_bloc/home_body_int.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeBodyInt extends StatefulWidget {
+  @override
+  _HomeBodyIntState createState() => _HomeBodyIntState();
+}
+
+class _HomeBodyIntState extends State<HomeBodyInt> {
+  RemoteConfig remoteConfig;
+
+  Future<RemoteConfig> initializeRemoteConfig() async {
+
+    /// Initialize remote config 
+    final RemoteConfig remoteConfig = await RemoteConfig.instance; 
+
+    /// Setting defaults values if don't catch from firebase host
+    final defaultValues = <String, dynamic>{"ExampleInt": 2};
+    await remoteConfig.setDefaults(defaultValues);
+
+    /// Fetch params for your app, by default the
+    /// expiration is 12 hours
+    await remoteConfig.fetch(expiration: Duration(seconds: 4));
+
+    /// Active fetch configs
+    await remoteConfig.activateFetched();
+
+    return remoteConfig;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: FutureBuilder<RemoteConfig>(
+          future: initializeRemoteConfig(),
+          builder: (BuildContext context, AsyncSnapshot<RemoteConfig> snapshot) {
+            return Center(
+              child: snapshot.hasData ? Text(snapshot.data.getInt("ExampleInt").toString()) : CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
