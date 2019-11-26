69a9dcb601381e27f669d287946a738d7d57903f
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-11-25 23:41:43 -0300

    Added 2 buttons to body

--- fire_analytics/lib/home/home_body.dart
+++ fire_analytics/lib/home/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:fire_analytics/home/home_bloc.dart';
 import 'package:fire_analytics/home/home_events.dart';
+import 'package:fire_analytics/sample_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,6 +24,15 @@ class _HomeBodyState extends State<HomeBody> {
           child: RaisedButton(
             onPressed: () => homeBloc.add(SendLogEvent(name: "Sample log", params: {"Hi":"test"})),
             child: Text("Send log"),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          width: 150,
+          height: 50,
+          child: RaisedButton(
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => SamplePage(), settings: RouteSettings(name: "/samplePage"))),
+            child: Text("Go to another page"),
           ),
         )
       ],
