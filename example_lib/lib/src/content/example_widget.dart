895127d50930aabef5f22cd510b8369cd9e803e0
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2020-12-21 23:23:27 -0300

    Finished project

--- example_lib/lib/src/content/example_widget.dart
+++ example_lib/lib/src/content/example_widget.dart
@@ -4,6 +4,8 @@ class ExampleWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 200,
+      width: 200,
       color: Colors.red,
     );
   }
